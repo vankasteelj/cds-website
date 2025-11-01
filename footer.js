5a2c1987ab4613f6082eada1e762b346175a0c6e
Jean van Kasteel <vankasteelj@gmail.com>
2025-11-01 14:49:41 +0100

    header now has a "dyslexic mode" button

--- footer.js
+++ footer.js
@@ -1,6 +1,6 @@
 const footer = `
 <div class="flex-container">
-  <div class="flex-item-left" id="facebook"><a href="https://www.facebook.com/Lecycledesonges" target="_blank"><img src="./images/facebook.png"></a></div>
+  <div class="flex-item-left" id="facebook"><a href="https://www.facebook.com/Lecycledesonges" target="_blank"><img src="./images/facebook.png" alt="Logo Facebook et lien vers la page de ce réseau social"></a></div>
   <div class="flex-item-center"><a href="mailto:lecycledessonges@gmail.com">lecycledessonges@gmail.com</a></div>
   <div class="flex-item-right"><a href="./qui-sommes-nous.html">Qui sommes-nous&nbsp;?</a></div>
 </div>
--- header.js
+++ header.js
@@ -1,10 +1,15 @@
 const header = `
 <a href="index.html">
   <div id="title">
-    <img src="./images/logo-full.png">
+    <img src="./images/logo-full.png" role="presentation" alt="">
     <h1>Le Cycle des Songes</h1>
   </div>
 </a>
+<label class="switch">
+  <input type="checkbox" id="dyslexic">
+  <span class="slider round"></span>
+  <span class="toggle-label">Mode dyslexique</span>
+</label>
 <nav>
   <a>Univers</a>
   <a href="timeline.html">Timeline</a>
@@ -15,3 +20,32 @@ const header = `
 `
 
 document.getElementById('header').innerHTML = header
+
+const onLoad = () => {
+  if (localStorage.dyslexic_mode === '1') {
+    $('#main').addClass('dyslexic')
+    document.getElementById('dyslexic').checked = true
+  }
+}
+
+const toggleDyslexic = () => {
+  if (localStorage.dyslexic_mode === '1') {
+    $('#main').addClass('dyslexic')
+    document.getElementById('dyslexic').checked = true
+  } else {
+    $('#main').removeClass('dyslexic')
+    localStorage.dyslexic_mode = '0'
+    document.getElementById('dyslexic').checked = false
+  }
+}
+
+$('#dyslexic').on('click', () => {
+  if (localStorage.dyslexic_mode === '1') {
+    localStorage.dyslexic_mode = '0'
+  } else {
+    localStorage.dyslexic_mode = '1'
+  }
+  toggleDyslexic()
+})
+
+onLoad()
