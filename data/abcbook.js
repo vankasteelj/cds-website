a1ad4d33cc4c7e37f9b99652be898e39a5a840ca
Jean van Kasteel <vankasteelj@gmail.com>
2025-11-02 17:19:29 +0100

    update abcbooks

--- data/abcbook.js
+++ data/abcbook.js
@@ -1,11 +1,123 @@
 const abcbook = {
+  a: [
+    {
+      word: `Amortel·le`,
+      desc: `Qualité des Égrégores : iels ne meurent pas et ne vieillissent pas naturellement, sauf si leur Manuscrit est détruit.`
+    },
+    {
+      word: `Antiquités`,
+      desc: `Objets issus de l’imaginaire collectif des Rêveur·ses, dotés de pouvoirs magiques variés. Contrairement aux Artefacts, elles ne sont liées à aucun·e Égrégore en particulier.`
+    },
+    {
+      word: `APR (Agence de Protection des Rêveur·ses)`,
+      desc: `Organisme indépendant chargé d’assurer la sécurité des Rêveur·ses et de protéger le Secret.`
+    },
+    {
+      word: `Artefact`,
+      desc: `Objet unique lié à un·e Égrégore, extension de son être, qui lui permet d’utiliser ses pouvoirs magiques.`
+    },
+    {
+      word: `Asphaleia`,
+      desc: `Courant politique égrégore. Comme les Révélationnistes, mais avec l’ambition de prendre le pouvoir sur les populations rêveuses, afin de rendre aux Égrégores leur place "légitime". La situation est stagnante depuis des siècles, rien ne bouge dans les Communautés ou dans les Cités et s’il faut passer par la force, qu’il en soit ainsi. `
+    }
+  ],
+  b: [
+    {
+      word: `Bibliothèque`,
+      desc: `Institution communautaire du Bureau des Songes qui protège les Manuscrits et surveille la production de Songes.`
+    },
+    {
+      word: `Bureau des Songes (BdS)`,
+      desc: `Institution communautaire chargée d’organiser la société égrégore et de maintenir l’ordre.`
+    }
+  ],
+  c: [
+    {
+      word: `Communautés`,
+      desc: `Groupes d’Égrégores définis par une homogénéité linguistique et/ou territoriale, chargés de gérer la population et le Secret.`
+    },
+    {
+      word: `Cités souterraines`,
+      desc: `Espaces aménagés en sous-sol pour accueillir les Égrégores non-intégrables (ENI) et leur permettre de vivre sans briser le Secret.`
+    },
+    {
+      word: `Conseil Suprême`,
+      desc: `Instance exécutive supracommunautaire composée des Président·es Communautaires.`
+    }
+  ],
+  d: [
+    {
+      word: `Direction de la Sécurisation`,
+      desc: `Branche du Bureau des Songes chargée du maintien de l’ordre et des enquêtes au sein de la Communauté.`
+    },
+    {
+      word: `Direction du Registre`,
+      desc: `Service administratif du Bureau des Songes qui gère la prise en charge des nouveaux Égrégores, les statuts civils et les ENI.`
+    },
+    {
+      word: `Direction Scientifique et Humanitaire`,
+      desc: `Organe scientifique du Bureau des Songes, chargé de la santé des Égrégores et de l’étude des Songes, Artefacts et Antiquités.`
+    },
+    {
+      word: `Direction des Cités Souterraines`,
+      desc: `Service du Bureau des Songes qui crée et administre les Cités souterraines, gère leurs ressources et leurs élections.`
+    },
+    {
+      word: `Direction de la Justice`,
+      desc: `Branche du Bureau des Songes qui organise les procès égrégores, tire au sort les jurys et agit comme ministère public.`
+    }
+  ],
+  e: [
+    {
+      word: `Égrégore`,
+      desc: `Personnage de fiction qui a ploppé dans le monde réel. Iel possède la personnalité et les souvenirs de son œuvre originale.`
+    },
+    {
+      word: `Égrégores non-intégrables (ENI)`,
+      desc: `Égrégores dont l’apparence ou la psychologie empêche l’intégration dans la société des Rêveur·ses. Ils vivent dans les Cités souterraines.`
+    },
+    {
+      word: `Enfants`,
+      desc: `Les Égrégores sont stériles et ne peuvent pas procréer. Toutefois, si un personnage est enfant dans son œuvre originale, il peut plopper sous cette forme et grandir normalement jusqu’à l’âge adulte, moment où il arrête de vieillir.`
+    },
+    {
+      word: `Éveillé·e`,
+      desc: `Rêveur·se qui est au courant de l'existence des Égrégores (le Secret est brisé). Iel ne génère dès lors plus de songes.`
+    }
+  ],
+  f: [
+    {
+      word: `Fondamentalistes`,
+      desc: `Courant politique majoritaire chez les Égrégore. Le Secret avant tout. Les Cités Souterraines restent la seule vraie (et la moins pire) solution. C'est également le parti qui a mené plusieurs réformes au cours des dernières décennies et siècles.`
+    }
+  ],
+  i: [
+    {
+      word: `Interventionnistes`,
+      desc: `Courant politique du Parti Fondamentaliste. Les interventionnistes sont une branche au sein des fondamentalistes qui prône une intervention active et directe (tout en conservant le Secret) des Égrégores dans les affaires rêveuses (politiques entre autres). `
+    },
+    {
+      word: `Isolationnistes`,
+      desc: `Courant politique égrégore. Leur objectif : créer un territoire à part, coupé du monde rêveur. ENI et intégrables y vivraient au grand jour, sans contact avec le monde rêveur, avec un risque de rupture du Secret.`
+    },
+  ],
+  j: [
+    {
+      word: `Jury`,
+      desc: `Au niveau communautaire, groupe de dix Égrégores tiré·es au hasard pour juger les affaires judiciaires. Les procès sont gérés par la Direction de la Justice du Bureau des Songes.`
+    }
+  ],
   m: [
     {
       word: `Manuscrit`,
       desc: `C’est le premier objet physique dans lequel l’auteur·rice a mis son idée complète du personnage. Parfois il s’agit d’un brouillon, parfois du premier livre imprimé, de la première esquisse d’un tableau, etc. Et parfois… on ne sait pas. C’est ce qu’on appelle des “Manuscrits perdus”.<br>Pourquoi ces Manuscrits sont-ils si importants&nbsp;? C’est parce qu’ils sont le vecteur des Songes vers l’Égrégore. C’est un peu l'entonnoir dans lequel les Songes se rassemblent et puis vont vers l’Egrégore à partir du moment où iel ploppe. Ils sont donc extrêmement précieux. <br>Ils sont rassemblés dans la Bibliothèque, qui est une organisation indépendante (et dont il existe une antenne dans la plupart des Communautés). Un de ses services est d’ailleurs totalement dédié à la recherche des Manuscrits perdus.`
     },
     {
-      word: `Marrainage`,
+      word: `Mariage`,
+      desc: `Union entre Égrégores qui a pour effet de mettre en commun leurs Songes.`
+    },
+    {
+      word: `Marrainage/Parrainage`,
       desc: `Le fait qu’un·e Égrégore expérimenté·e offre du soutien et de l’accompagnement à un·e néoploppé·e.`
     }
   ],
@@ -23,12 +135,24 @@ const abcbook = {
   ],
   p: [
     {
+      word: `Parlement Communautaire`,
+      desc: `Instance législative au niveau communautaire, instituant les lois égrégores de la Communauté. Des représentant·es élu·es sont envoyé·es au Parlement Suprême.`
+    },
+    {
+      word: `Parlement Suprême`,
+      desc: `Instance législative supracommunautaire et internationale définissant les grandes règles de la société égrégore.`
+    },
+    {
       word: `Ploppage`,
       desc: `(verbe : plopper) C’est le moment où l’Égrégore apparaît dans le monde physique, sorti·e de nulle part.`
     },
     {
-      word: `plopper`,
-      desc: `Se dit des Egrégores quand ielles apparaissent de nulle part dans le monde réel pour la première fois.` 
+      word: `Plopper`,
+      desc: `Se dit des Egrégores quand ielles apparaissent de nulle part dans le monde réel pour la première fois.`
+    },
+    {
+      word: `Président·e Communautaire`,
+      desc: `Responsable exécutif·ve d’une Communauté, chargé·e de mettre en œuvre les lois et de gérer les relations intercommunautaires.`
     }
   ],
   r: [
@@ -39,12 +163,26 @@ const abcbook = {
     {
       word: `Rêveur·ses`,
       desc: `Êtres humains lambda.`
+    },
+    {
+      word: `Révélationnistes`,
+      desc: `Courant politique égrégore prônant la révélation de l’existence des Égrégores aux Rêveur·ses. Révélons le Secret ! Oui, on bloque la naissance de futur·es Égrégores, mais autant permettre aux Égrégores intégrables et non intégrables existant·es de vivre de manière décente et au grand jour. Les ENI pourront donc vivre au grand jour, comme le reste de la Communauté. `
     }
   ],
   s: [
     {
       word: `Songes`,
       desc: `C’est un concept encore méconnu. C’est une sorte d’énergie que les Rêveur·ses génèrent lorsqu’ielles croient avec suffisamment de passion en un être fictif. Il ne s’agit pas uniquement de “connaître” le personnage mais de l’aimer et de se laisser emporter par son histoire.`
+    },
+    {
+      word: `Secret`,
+      desc: `Principe fondamental de la société égrégore : car si un·e Rêveur·se découvre l’existence des Égrégores et devient Éveillé·e, iel ne peut plus générer de Songes. Les Égrégores ne peuvent donc plus replopper ni utiliser leurs pouvoirs. Si le Secret est brisé, plus aucun Égrégores ne pourra plopper.`
+    }
+  ],
+  v: [
+    {
+      word: `Vie quotidienne`,
+      desc: `Les Égrégores doivent régulièrement déménager de Communauté en Communauté pour ne pas éveiller les soupçons des Rêveur·ses, car iels ne vieillissent pas. Certain·es travaillent dans les institutions égrégores, d’autres s’intègrent dans la société humaine.`
     }
   ]
 }
