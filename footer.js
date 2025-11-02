3b4f9d6960d1067ceffbcfbdee617d2d5ce13466
Jean van Kasteel <vankasteelj@gmail.com>
2025-11-02 20:07:12 +0100

    kinda obfuscate email

--- footer.js
+++ footer.js
@@ -1,7 +1,8 @@
+const mail = 'lecycledessonges'+'@'+'gmail.com'
 const footer = `
 <div class="flex-container">
   <div class="flex-item-left" id="facebook"><a href="https://www.facebook.com/Lecycledesonges" target="_blank"><img src="./images/facebook.png" alt="Logo Facebook et lien vers la page de ce réseau social"></a></div>
-  <div class="flex-item-center"><a href="mailto:lecycledessonges@gmail.com">lecycledessonges@gmail.com</a></div>
+  <div class="flex-item-center"><a href="mailto:${mail}">${mail}</a></div>
   <div class="flex-item-right"><a href="./equipe.html">Qui sommes-nous&nbsp;?</a></div>
 </div>
 <p id="madeby">&copy; ${new Date().getFullYear()} Les Chats-pitrés - Site web créé avec &hearts; par <a href="https://github.com/vankasteelj" target="_blank">Jean van Kasteel<a></p>
