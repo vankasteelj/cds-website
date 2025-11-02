d843c62b7a5151985577c8044c086d7626f4ca23
Jean van Kasteel <vankasteelj@gmail.com>
2025-11-02 16:37:24 +0100

    remove égide from footer

--- footer.js
+++ footer.js
@@ -4,7 +4,7 @@ const footer = `
   <div class="flex-item-center"><a href="mailto:lecycledessonges@gmail.com">lecycledessonges@gmail.com</a></div>
   <div class="flex-item-right"><a href="./equipe.html">Qui sommes-nous&nbsp;?</a></div>
 </div>
-<p id="madeby">&copy; ${new Date().getFullYear()} Les Chats-pitrés - Sous l'égide de <a href="https://etencore-asbl.org/" target="_blank">Et Encore... asbl</a> - Site web créé avec &hearts; par <a href="https://github.com/vankasteelj" target="_blank">Jean van Kasteel<a></p>
+<p id="madeby">&copy; ${new Date().getFullYear()} Les Chats-pitrés - Site web créé avec &hearts; par <a href="https://github.com/vankasteelj" target="_blank">Jean van Kasteel<a></p>
 `
 
 document.getElementById('footer').innerHTML = footer
