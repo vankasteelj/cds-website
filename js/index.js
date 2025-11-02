19afc050b170ea87f27eb25f6a00d4b15c75e27d
Jean van Kasteel <vankasteelj@gmail.com>
2025-11-02 15:48:05 +0100

    remove intrusive countdown

--- js/index.js
+++ js/index.js
@@ -8,19 +8,15 @@ for (let i in events) {
       <span class="subtitle">${event.subtitle}</span>
       <span class="date" data="${event.date}">${event.displayDate}</span>
     </div>
-    <div class="countdown">
-      <span class="days">00</span> jour(s) <span class="hours">00</span>h <span class="minutes">00</span>min <span class="seconds">00</span>sec
-    </div>
+    <div class="message"></div>
   </div>`)
 }
-
-const pad = (n) => n < 10 ? '0' + n : n
 
 $('.event').each((idx) => {
   const d = $($('.event .date')[idx]).attr('data')
 
   if (!d) {
-    $($('.event .countdown')[idx]).html(`À venir`).show()
+    $($('.event .message')[idx]).html(`À venir`).show()
     return true
   }
 
@@ -28,22 +24,11 @@ $('.event').each((idx) => {
   const today = new Date().getTime()
 
   if ((date - today < 0) && (date - today > -(1000*60*60*24))) {
-    $($('.event .countdown')[idx]).html(`C'est aujourd'hui`).show()
+    $($('.event .message')[idx]).html(`C'est aujourd'hui`).show()
   } else if (date - today > 0) {
-    const time = () => {
-      const distance = date - new Date().getTime()
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      $($('.event .countdown')[idx]).find('.days').text(pad(days))
-      $($('.event .countdown')[idx]).find('.hours').text(pad(hours))
-      $($('.event .countdown')[idx]).find('.minutes').text(pad(minutes))
-      $($('.event .countdown')[idx]).find('.seconds').text(pad(seconds))
-    }
-    time()
-    setInterval(time, 1000)
-    $($('.event .countdown')[idx]).show()
+    const distance = date - new Date().getTime()
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    $($('.event .message')[idx]).html(`Plus que ${days} ${days > 1 ? 'dodos':'dodo'}`).show()
   } else {
     $($('.event')[idx]).append(`<div class="red-corner"></div>
       <div class="ended">Terminé</div>`)
